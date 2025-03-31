54bbcddc746bf21cd2b055532f2d46ae5d48d7f9
Kawan <kawan_weber@hotmail.com>
2025-03-31 08:56:38 -0300

    Finalizar

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+double milhas;
+double km;
+
+Console.WriteLine("\t\t\tConversor de Medidas: Milhas para Km");
+
+Console.WriteLine("\nDigite o valor em Milhas:");
+milhas = Convert.ToDouble(Console.ReadLine());
+
+km = milhas * 1609;
+
+Console.WriteLine($"Aqui está o valor convertido: {km} Quilômetros");
+
+
